c1e8620c026e6eb2a79910f40d57c1124709fb8e
Afrohead01 <subhamdeb409@gmail.com>
2022-11-29 17:15:57 +0530

    Url

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const http = require('http');
-const server = http.createServer((req, res)=>{
-    console.log('Subham');
+
+ server = http.createServer((req, res)=>{
+    const url = req.url
+    if (url==='/'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title></title><head>')
+        res.write('<body><form action="/node" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title></title><head>')
+    res.write('<body><h1>Welcome To my Node JS project!</h1></body>')
+    res.write('</html>')
+    res.end()
 });
+
+
 
 server.listen(3000)
